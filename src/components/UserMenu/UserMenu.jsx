db49454f12d3b194c0190795e551e28929ec24cc
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-04 20:56:53 +0300

    Update UserMenu.jsx

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutUser, token } from 'redux/authSlice';
+import { UserWrapperDiv, UserH2, Button } from './UserMenu.styled';
+
+const UserMenu = () => {
+  const userEmail = useSelector(state => state.auth.user.email);
+  const tokenAuth = useSelector(state => state.auth.token);
+  const dispatch = useDispatch();
+  const onClickHandle = () => {
+    token.set(tokenAuth);
+    dispatch(logoutUser());
+  };
+  return (
+    <UserWrapperDiv>
+      <UserH2>{userEmail}</UserH2>
+      <Button className="button" type="button" onClick={onClickHandle}>
+        Logout
+      </Button>
+    </UserWrapperDiv>
+  );
+};
+export default UserMenu;
