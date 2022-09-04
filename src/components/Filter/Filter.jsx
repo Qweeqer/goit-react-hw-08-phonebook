ee83bd6f55463cd65bd0519eab6d092fc4aa0940
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-04 21:03:41 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,46 @@
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 
-import { filteredContacts, getFilter } from '../../redux/filterSlice';
+// import { filteredContacts, getFilter } from '../../redux/filterSlice';
 
+// import './Filter.module.css';
+
+// export const Filter = () => {
+//   const filter = useSelector(getFilter);
+//   const dispatch = useDispatch();
+//   return (
+//     <label>
+//       <input
+//         type="text"
+//         name="filter"
+//         value={filter}
+//         onChange={e => dispatch(filteredContacts(e.target.value))}
+//         placeholder="Find contacts by name or phone"
+//       />
+//     </label>
+//   );
+// };
+import React from 'react';
 import './Filter.module.css';
+import s from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { changeFilterValue } from '../../redux/contactsSlice';
 
-export const Filter = () => {
-  const filter = useSelector(getFilter);
+const Filter = () => {
+  const filterValue = useSelector(state => state.filterValue.value);
   const dispatch = useDispatch();
   return (
-    <label>
+    <div className={s.filterContainer}>
+      <p>Find contacts by name</p>
       <input
-        type="text"
+        className={s.filterInput}
         name="filter"
-        value={filter}
-        onChange={e => dispatch(filteredContacts(e.target.value))}
-        placeholder="Find contacts by name or phone"
-      />
-    </label>
+        value={filterValue}
+        onChange={e => {
+          dispatch(changeFilterValue(e.currentTarget.value));
+        }}
+      ></input>
+    </div>
   );
 };
+
+export default Filter;
