d685cb8e1c170c8e9d58374b51baa563db03fb41
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-04 20:45:13 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,44 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { contactsReducer } from './contactsSlice';
-import { filter } from './filterSlice';
-import { contactsApi } from '../services/contactsAPI';
+import authSlice from './authSlice';
+import { contactsApi } from '../services/contactsApi';
+import filterValueReducer from './contactsSlice';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, authSlice);
 
 export const store = configureStore({
   reducer: {
+    auth: persistedReducer,
+    filterValue: filterValueReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
-    filter: filter.reducer,
   },
-
   middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
     contactsApi.middleware,
   ],
 });
+
+export let persistor = persistStore(store);
+setupListeners(store.dispatch);
