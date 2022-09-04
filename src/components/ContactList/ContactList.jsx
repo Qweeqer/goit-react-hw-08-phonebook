efc91e360ad18a145cca08485f2f59b5c88d9e8c
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-04 21:06:33 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,40 +1,94 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-import { getFilter } from '../../redux/filterSlice';
+// import { getFilter } from '../../redux/filterSlice';
+// import {
+//   useFetchContactsQuery,
+//   useDeleteContactMutation,
+// } from '../../services/contactsAPI';
+
+// import css from './ContactList.module.css';
+
+// export const ContactList = () => {
+//   const { data: contacts } = useFetchContactsQuery();
+//   const filter = useSelector(getFilter);
+//   const [deleteContact] = useDeleteContactMutation();
+
+//   const filterContacts = contacts?.filter(
+//     contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+//       contact.phone.includes(filter)
+//   );
+//   return (
+//     <ul>
+//       {contacts &&
+//         filterContacts.map(({ id, name, phone }) => (
+//           <li key={id}>
+//             <p>
+//               {name}: {phone}
+//             </p>
+//             <button
+//               className={css.listDeleteButton}
+//               type="button"
+//               onClick={() => deleteContact(id)}
+//             >
+//               Delete
+//             </button>
+//           </li>
+//         ))}
+//     </ul>
+//   );
+// };
+import * as React from 'react';
+// import css from './ContactList.module.css';
+import './ContactList.css';
+import { useSelector } from 'react-redux/es/exports';
 import {
-  useFetchContactsQuery,
+  useGetContactsQuery,
   useDeleteContactMutation,
-} from '../../services/contactsAPI';
+} from 'services/contactsApi';
+import { Section, H2 } from './ContactList.styled';
 
-import css from './ContactList.module.css';
+function ContactList() {
+  const { data } = useGetContactsQuery();
+  const [deleteContact, { isFetching }] = useDeleteContactMutation();
 
-export const ContactList = () => {
-  const { data: contacts } = useFetchContactsQuery();
-  const filter = useSelector(getFilter);
-  const [deleteContact] = useDeleteContactMutation();
+  const filterValue = useSelector(state => state.filterValue.value);
 
-  const filterContacts = contacts?.filter(
-    contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-      contact.phone.includes(filter)
-  );
+  const getFilteredContacts = () => {
+    if (!data) return;
+    const filteredContactsArray = [...data].filter(contact =>
+      contact.name.toLowerCase().includes(filterValue.toLowerCase())
+    );
+    return filteredContactsArray;
+  };
+
   return (
-    <ul>
-      {contacts &&
-        filterContacts.map(({ id, name, phone }) => (
-          <li key={id}>
-            <p>
-              {name}: {phone}
-            </p>
-            <button
-              className={css.listDeleteButton}
-              type="button"
-              onClick={() => deleteContact(id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-    </ul>
+    <>
+      <Section className="contacts">
+        <H2 className="contacts__title">Contacts</H2>
+        <ul className="contacts">
+          {data &&
+            getFilteredContacts().map(({ name, number, id }) => {
+              return (
+                <li key={id} className="contacts__item">
+                  <p>
+                    • {name} : {number}
+                  </p>
+                  <button
+                    type="button"
+                    className="contacts__del-btn"
+                    onClick={() => deleteContact(id)}
+                    disabled={isFetching}
+                  >
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+        </ul>
+      </Section>
+    </>
   );
-};
+}
+
+export default ContactList;
