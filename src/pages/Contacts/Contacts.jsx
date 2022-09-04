bd778f267f12aae70db298f55aefed78c153a8ba
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-04 20:26:11 +0300

    Update Contacts.jsx

---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,17 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+
+const Contacts = () => {
+  return (
+    <>
+      <h1>Add new contact</h1>
+      <ContactForm />
+
+      <Filter />
+      <ContactList />
+    </>
+  );
+};
+
+export default Contacts;
