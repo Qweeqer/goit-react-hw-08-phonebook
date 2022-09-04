9df363219c1c4a4f1245cc2aca8e944e05b101b2
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 01:04:53 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -69,6 +69,7 @@
 // };
 import { useState } from 'react';
 import './ContactForm.module.css';
+import '../App.module.css';
 import {
   useAddContactMutation,
   useGetContactsQuery,
@@ -108,33 +109,28 @@ const ContactForm = () => {
 
   return (
     <form className="contacts-form" onSubmit={handleSubmit}>
-      Name
-      <label className="contacts-form__name-label">
-        <input
-          className="contacts-form__input"
-          type="text"
-          name="name"
-          value={formFields.name}
-          onChange={handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </label>
-      <label className="contacts-form__name-label">
-        Number
-        <input
-          className="contacts-form__input"
-          type="tel"
-          name="number"
-          value={formFields.number}
-          onChange={handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-      </label>
-      <button className="new-contact-btn" type="submit">
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={formFields.name}
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <input
+        type="tel"
+        name="number"
+        placeholder="Phone"
+        value={formFields.number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+
+      <button className="button" type="submit">
         Add contact
       </button>
     </form>
