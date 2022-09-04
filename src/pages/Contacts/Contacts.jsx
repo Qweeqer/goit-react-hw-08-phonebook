97e53e5bae9b5368fac755d81e33ace5720b2d67
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 01:05:33 +0300

    Update Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,16 +1,19 @@
 import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
+import '../../components/App.module.css';
 
 const Contacts = () => {
   return (
-    <>
-      <h1>Phonebook</h1>
-      <ContactForm />
-
-      <Filter />
-      <ContactList />
-    </>
+    <section>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    </section>
   );
 };
 
