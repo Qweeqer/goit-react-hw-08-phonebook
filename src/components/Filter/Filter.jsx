1c7ec2851a3aa2ef3b75035197d7a9703045e348
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 22:33:05 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,29 +1,9 @@
-// import { useSelector, useDispatch } from 'react-redux';
-
-// import { filteredContacts, getFilter } from '../../redux/filterSlice';
-
-// import './Filter.module.css';
-
-// export const Filter = () => {
-//   const filter = useSelector(getFilter);
-//   const dispatch = useDispatch();
-//   return (
-//     <label>
-//       <input
-//         type="text"
-//         name="filter"
-//         value={filter}
-//         onChange={e => dispatch(filteredContacts(e.target.value))}
-//         placeholder="Find contacts by name or phone"
-//       />
-//     </label>
-//   );
-// };
 import React from 'react';
-import './Filter.module.css';
-// import '../../components/ContactForm/ContactForm.module.css';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
+
 import { changeFilterValue } from '../../redux/contactsSlice';
+
+import './Filter.module.css';
 
 const Filter = () => {
   const filterValue = useSelector(state => state.filterValue.value);
