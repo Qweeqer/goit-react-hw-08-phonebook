e018b0c1e37342809346c510f245788881b01d67
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 22:33:32 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+
 import { logoutUser } from 'redux/authSlice';
 
 import s from './UserMenu.module.css';
