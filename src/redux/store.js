8adae2c62989e377cfe15bdf5ea87875d5328d72
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 16:24:26 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,7 +19,8 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['filter'],
+  blacklist: ['user'],
+  // blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, authSlice);
