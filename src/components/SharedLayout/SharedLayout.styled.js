1f01330911d8e43745f2a454e3ccf03382100ca1
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 16:23:37 +0300

    Update SharedLayout.styled.js

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -10,7 +10,7 @@ export const Container = styled.div`
 export const Header = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   gap: 12px;
   padding: 8px 0;
   margin-bottom: 16px;
@@ -33,6 +33,4 @@ export const NavLinkSt = styled(NavLink)`
   }
 `;
 
-export const Main = styled.main`
-  background-image: url('../../images/homefon.jpg');
-`;
+
