c66807d26ce54545c5af318dc2beb477e61beca9
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 16:23:55 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
-import { H1 } from './Home.styled';
+import { H1, P } from './Home.styled';
 import '../../components/ContactForm/ContactForm.module.css';
+import s from './Home.module.css';
 
 export default function Home() {
   return (
-    <section>
+    <section className={s.homePage}>
       <form>
         <H1>Welcome to Phonebook application</H1>
+      </form>
+      <form>
+        <P className="appDescription">
+          PhoneBook app is an easy to use contact manager app that gives you
+          facility of saving and viewing your contacts, so that you never lose
+          your contacts.
+        </P>
       </form>
     </section>
   );
