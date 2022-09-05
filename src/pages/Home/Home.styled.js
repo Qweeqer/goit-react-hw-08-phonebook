34e5a2c7819ce4261b6baa159e93eca04aeaac29
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 09:22:16 +0300

    Update Home.styled.js

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const H1 = styled.h1`
   font-size: 60px;
   text-aligh: center;
-  padding-top: 50px;
-  color: blue;
+  // padding-top: 50px;
+  color: #ffffff;
+  // background-image: url(./images/homefon.jpg);
 `;
