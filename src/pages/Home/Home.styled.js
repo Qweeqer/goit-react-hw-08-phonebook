c9bcdd38c26c68bcc2a1673c4802266fea438e46
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 16:24:05 +0300

    Update Home.styled.js

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -5,5 +5,25 @@ export const H1 = styled.h1`
   text-aligh: center;
   // padding-top: 50px;
   color: #ffffff;
-  // background-image: url(./images/homefon.jpg);
+  text-shadow: 0 1px 0 #cac8c8, 0 1px 0 #c7c5c5, 0 1px 0 #bbbaba,
+    0 1px 0 #b7b6b6, 0 1px 0 #a9a6a6, 0 6px 1px rgb(0 0 0 / 20%),
+    0 0px 5px rgb(0 0 0 / 10%), 0 1px 3px rgb(0 0 0 / 10%),
+    0 3px 2px rgb(0 0 0 / 10%), 0 5px 2px rgb(0 0 0 / 20%),
+    0 5px 5px rgb(0 0 0 / 30%), 0 5px 5px rgb(0 0 0 / 10%);
+`;
+export const P = styled.p`
+  font-size: 24px;
+  text-aligh: center;
+  padding: 30px;
+  color: #ffffff;
+  padding: 20px 50px 20px 50px;
+  width: 70%;
+  letter-spacing: 2px;
+  text-shadow: 0 1px 0 #cac8c8, 0 1px 0 #c7c5c5, 0 1px 0 #bbbaba,
+    0 1px 0 #b7b6b6, 0 1px 0 #a9a6a6, 0 6px 1px rgb(0 0 0 / 20%),
+    0 0px 5px rgb(0 0 0 / 10%), 0 1px 3px rgb(0 0 0 / 10%),
+    0 3px 2px rgb(0 0 0 / 10%), 0 5px 2px rgb(0 0 0 / 20%),
+    0 5px 5px rgb(0 0 0 / 30%), 0 5px 5px rgb(0 0 0 / 10%);
+
+  background-image: url(../../images/homefon.jpg);
 `;
