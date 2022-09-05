863eff9b1b9d6366d9e675593c47e78de078d038
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 22:32:53 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,45 +1,6 @@
-// import { useSelector } from 'react-redux';
-
-// import { getFilter } from '../../redux/filterSlice';
-// import {
-//   useFetchContactsQuery,
-//   useDeleteContactMutation,
-// } from '../../services/contactsAPI';
-
-// import css from './ContactList.module.css';
-
-// export const ContactList = () => {
-//   const { data: contacts } = useFetchContactsQuery();
-//   const filter = useSelector(getFilter);
-//   const [deleteContact] = useDeleteContactMutation();
-
-//   const filterContacts = contacts?.filter(
-//     contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-//       contact.phone.includes(filter)
-//   );
-//   return (
-//     <ul>
-//       {contacts &&
-//         filterContacts.map(({ id, name, phone }) => (
-//           <li key={id}>
-//             <p>
-//               {name}: {phone}
-//             </p>
-//             <button
-//               className={css.listDeleteButton}
-//               type="button"
-//               onClick={() => deleteContact(id)}
-//             >
-//               Delete
-//             </button>
-//           </li>
-//         ))}
-//     </ul>
-//   );
-// };
 import * as React from 'react';
 import { useSelector } from 'react-redux/es/exports';
+
 import {
   useGetContactsQuery,
   useDeleteContactMutation,
