0d591ace9071c3682c6cf497d3e73c3a696d4837
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-05 09:22:08 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,5 +2,11 @@ import { H1 } from './Home.styled';
 import '../../components/ContactForm/ContactForm.module.css';
 
 export default function Home() {
-  return <H1>Welcome to Phonebook application</H1>;
+  return (
+    <section>
+      <form>
+        <H1>Welcome to Phonebook application</H1>
+      </form>
+    </section>
+  );
 }
